cdb0fbf7f3b0d9e6f582866e003ee4de1727f85c
uchihaashar <mohdasharshakeel@gmail.com>
2024-11-05 18:27:32 +0530

    fetching data from API

--- src/components/templates/SideNav.jsx
+++ src/components/templates/SideNav.jsx
@@ -8,26 +8,26 @@ const SideNav = () => {
   return (
     <div className='w-[20%] h-full border-r-2 border-zinc-200 p-10 overflow-auto'>
  <h1 className='font-bold text-2xltext-white'>
- <i class="text-[#6556CD] ri-tv-fill text-2xl"></i>
+ <i className="text-[#6556CD] ri-tv-fill text-2xl"></i>
   <span className='text-2xl'>AMP</span> 
  </h1>
  
 <nav className='flex flex-col gap-3 text-xl text-zinc-400'>
 <h1 className='mt-10 mb-5 text-xl font-semibold text-white '> New feed</h1>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class=" mr-2 ri-fire-fill"></i>
+<i classname=" mr-2 ri-fire-fill"></i>
 Trending</Link>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class="mr-2 mt-2 ri-magic-fill"></i>
+<i classname="mr-2 mt-2 ri-magic-fill"></i>
 Popular</Link>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class="mr-2 mt-2 ri-movie-2-fill"></i>
+<i classname="mr-2 mt-2 ri-movie-2-fill"></i>
 Movies</Link>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class="mr-2 mt-2 ri-tv-2-fill"></i>
+<i classname="mr-2 mt-2 ri-tv-2-fill"></i>
 TV Shows</Link>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class="mr-2 mt-2 ri-team-fill"></i>
+<i classname="mr-2 mt-2 ri-team-fill"></i>
 People</Link>
 </nav> 
 <hr  className='border-none h-[1px] bg-zinc-400'/>
@@ -35,10 +35,10 @@ People</Link>
 <nav className='flex flex-col gap-3 text-xl text-zinc-400'>
 <h1 className='mt-10 mb-5 text-xl font-semibold text-white '> Web Information</h1>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class=" mr-2 ri-information-fill"></i>
+<i className="mr-2 ri-information-fill"></i>
 About</Link>
 <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5'>
-<i class="mr-2 mt-2 ri-phone-fill"></i>
+<i className="mt-2 mr-2 ri-phone-fill"></i>
 Contact Us</Link>
 
 </nav> 
